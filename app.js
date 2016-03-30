12045e9e28c13b3f97dae88c14cc6005a5fa0076
tracyalison11 <tracyak13@gmail.com>
2016-03-29 21:06:53 -0700

    Updated app using ng-change function

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-'use strict';
 
-var selectedFont;
+var radioForm = angular.module('radioForm', []);
+
+    radioForm.controller('fontController', function($scope){
+        $scope.formData = {
+        };
+
+        $scope.changeRadio = function() {
+            switch ($scope.formData.font){
+                case "fairytales":
+                    $('input[type=text], textarea').css('font-family', 'fairytales');
+                    break;
+                case "stump-closed":
+                    $('input[type=text], textarea').css('font-family', 'stump-closed');
+                    break;
+                case "amelia":
+                    $('input[type=text], textarea').css('font-family', 'Amelia-script');
+                    break;
+                case "brixton-lt":
+                    $('input[type=text], textarea').css('font-family', 'brixton-lt');
+                    break;
+                case "grounday":
+                    $('input[type=text], textarea').css('font-family', 'brixton-lt');
+                    break;
+                default:
+                    $('input[type=text], textarea').css('font-family', 'sans-serif');
+
+            }
+        };
+
+    });
